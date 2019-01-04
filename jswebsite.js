2d1188498b5a53b5be8e6a8b9e97918ee86426f1
lightBulb17 <lightBulb17@github.com>
2019-01-04 17:56:23 -0500

    1) initalstartup
    starting new website project.

---
+++ jswebsite.js
@@ -0,0 +1,24 @@
+// this will be the start of the project
+// header 
+    // inside the header
+    // navigation bar 
+        // Home
+        // photo of the day
+        // girls
+        // guys
+        // contact us 
+        // sponsors(for future people who want to get their name on our page)
+        // enter for chance to win #store as a hashmap so there are no duplicates (only supply instagram names)
+        
+
+//body 
+    // inside the body
+    // photo of the day 
+
+    // current girls
+    // current guys 
+
+
+    
+//footer 
+
